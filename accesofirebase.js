c71f106b718f63dc5cd31ffd4671865433840d3b
Diego H.L <diegoherreralopez9@gmail.com>
2025-07-06 12:36:26 -0600

    creación del proyecto
    se creo el proyecto y se agrego el accesofirebase

---
+++ accesofirebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBdJKOGPbb-mcUubwt8W5JVPVlJ_RTQ6jQ",
+  authDomain: "activida-59202.firebaseapp.com",
+  projectId: "activida-59202",
+  storageBucket: "activida-59202.firebasestorage.app",
+  messagingSenderId: "1044061249675",
+  appId: "1:1044061249675:web:d7aa9530b1db64055e348e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;

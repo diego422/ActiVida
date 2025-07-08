fd1c93e6b700ae90b45a0ba8efaa515069c28984
Diego H.L <diegoherreralopez9@gmail.com>
2025-07-07 23:28:25 -0600

    se agregaron los enlances
    se agrego en el appStack addExrcise y EditExercise

--- navigation/AppStack.js
+++ navigation/AppStack.js
@@ -3,6 +3,8 @@ import NavTab from "./NavTab";
 import ModifyPlan from "../screens/modifyPlan";
 import viewUser from "../screens/viewUser";
 import AwardsPage from "../screens/awards";
+import EditExercise from "../screens/EditExercise";
+import AddExercise from "../screens/AddExercise";
 const Stack = createNativeStackNavigator();
 
 export default function AppStack() {
@@ -10,6 +12,8 @@ export default function AppStack() {
         <Stack.Navigator screenOptions={{ headerShown: false }}>
             <Stack.Screen name="Tabs" component={NavTab} />
             <Stack.Screen name="ModifyPlan" component={ModifyPlan} />
+            <Stack.Screen name="EditExercise" component={EditExercise} />
+            <Stack.Screen name="AddExercise" component={AddExercise} />
             <Stack.Screen name="ViewUserInfo" component={viewUser} />
             <Stack.Screen name="awards" component={AwardsPage} />
             {/* Falta añadir el de usuario acá */}
